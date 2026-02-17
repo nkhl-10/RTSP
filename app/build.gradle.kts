3b0ac5aa4c02735a84ec45b878df7180b896cfb0
Nikhil Prajapati <nikhil.prajapati@teksun.com>
2026-02-17 12:55:38 +0530

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,7 @@ plugins {
 
 android {
     namespace = "com.android.rtsp"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk =  36
 
     defaultConfig {
         applicationId = "com.android.rtsp"
